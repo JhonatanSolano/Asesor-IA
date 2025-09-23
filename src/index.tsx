4973872a29b104583e2c8271d2c3b44f7ac626b2
Jhonatan Alejandro Solano Mendoza <jhsolanom@unal.edu.co>
2025-09-23 17:17:15 -0500

    Add src folder and index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,11 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
+if (!apiKey) {
+  throw new Error("API key no encontrada");
+}
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
